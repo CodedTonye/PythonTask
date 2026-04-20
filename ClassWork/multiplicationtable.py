8e59640cca5d3f1e8d5caf323e8ab3fc953b8095
CodedTonye <tonyefredhorsfall101@gmail.com>
2026-04-20 10:41:43 +0100

    Add new file

---
+++ ClassWork/multiplicationtable.py
@@ -0,0 +1,13 @@
+#PSEUDOCODE:
+
+#Step 1: Collect user input
+#Step 2: Declare a variable and initialize to one
+#Step 3: Use 'for' statement and set condition to loop 10 times
+#Step 4: 
+
+number = int(input('Enter a number to multiply: '))
+    
+for count in range (1, 11):
+      print(f'{number} x {count} = {number * count}')  
+            
+    
